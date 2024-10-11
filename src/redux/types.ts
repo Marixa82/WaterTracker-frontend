8759ba9dacf01969544b3b95c41acb9c5c4da8e4
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-11 20:43:47 +0300

    create type.ts

---
+++ src/redux/types.ts
@@ -0,0 +1,15 @@
+
+export interface Credentials {
+    email: string;
+    password: string;
+    name?: string; // Опціональне поле, якщо використовується для реєстрації
+  }
+  
+  export interface SignInResponse {
+    token: string;
+    userData: {
+      name: string;
+      email: string;
+    };
+  }
+  
