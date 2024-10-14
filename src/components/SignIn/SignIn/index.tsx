af9f7f9890293f35a9df8417cf81a4d855d54619
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-14 23:48:24 +0300

    upd index.ts

--- src/components/SignIn/SignIn/index.tsx
+++ src/components/SignIn/SignIn/index.tsx
@@ -1,16 +1,55 @@
-import  { ReactNode } from 'react';
+import { UseAppDispatch } from '../../../hook';
+import { authOperations } from '../../../redux/auth'
 
-interface SignInProps {
-  children: ReactNode;
-}
+const SignForm: React.FC = () => {
+  const dispatch = UseAppDispatch();
 
-function SignIn({ children }: SignInProps) {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const email = (form.elements.namedItem('userEmail') as HTMLInputElement).value;
+    const password = (form.elements.namedItem('userPassword') as HTMLInputElement).value;
+
+    const formData = {
+      email,
+      password,
+    };
+
+    dispatch(authOperations.signIn(formData));
+  };
+
   return (
-    <>
-      <h1>SignIn</h1>
-      {children}
-    </>
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <label>
+        <span>
+          <input type="email" name="userEmail" placeholder="Enter your email..." required />
+        </span>
+      </label>
+      <label>
+        <span>
+          <input type="password" name="userPassword" placeholder="Enter your password..." required minLength={7} />
+        </span>
+      </label>
+      <button type="submit">Login</button>
+    </form>
   );
-}
+};
 
-export default SignIn;
+export default SignForm;
+
+// import  { ReactNode } from 'react';
+
+// interface SignInProps {
+//   children: ReactNode;
+// }
+
+// function SignIn({ children }: SignInProps) {
+//   return (
+//     <>
+//       <h1>SignIn</h1>
+//       {children}
+//     </>
+//   );
+// }
+
+// export default SignIn;
