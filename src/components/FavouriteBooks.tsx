3e708840b32ca34f472819b62d1f25576a491553
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-18 22:41:45 +0300

    upd FavoriteBooks

--- src/components/FavouriteBooks.tsx
+++ src/components/FavouriteBooks.tsx
@@ -1,7 +1,12 @@
-const favouriteBooks = [
+interface Book {
+  id: string;
+  name: string;
+}
+const favouriteBooks: Book[] = [
     { id: "id-1", name: "JS for beginners" },
     { id: "id-2", name: "React basics" },
     { id: "id-3", name: "React Router overview" },
     { id: "id-4", name: "Redux in depth" },
   ];
+  
   export default favouriteBooks;
