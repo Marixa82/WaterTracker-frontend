893d689a2123d214d44ddfd08329b6f670f48e17
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-18 22:44:35 +0300

    update

--- src/hook.ts
+++
@@ -1,7 +0,0 @@
-import {useSelector, TypedUseSelectorHook, useDispatch } from "react-redux";
-import type { AppDispatch, RootState } from './redux/store';
-
-// Для коректного визначення типів у всіх подіях 
-export const UseAppDispatch = () => useDispatch<AppDispatch>(); 
-// для відстеження всіх подій і структури у додатку
-export const UseAppSelector : TypedUseSelectorHook<RootState> = useSelector;
