651f6fe21533a5efa567aecd13ec48fba2a649cb
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-21 20:55:03 +0300

    create TrackerBenefitsList

---
+++ src/components/Welcome/TrackerBenefitsList/index.tsx
@@ -0,0 +1,15 @@
+
+import TrackerBenefits from '../TrackerBenefits';
+
+
+
+function TrackerBenefitsList() {
+  return (
+    <>
+      <h1>TrackerBenefitsList</h1>
+     <TrackerBenefits/>
+    </>
+  );
+}
+
+export default TrackerBenefitsList;
