b178ed94b9228bb1e596f9a94e124a72849d9388
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-22 18:00:18 +0300

    upd Icon.tsx

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import icons from '../assets/icons.svg'
 
 interface IconProps {
-  iconId: string;  // Ідентифікатор символу з спрайту
-  width?: number;  // Ширина іконки
-  height?: number; // Висота іконки
-  color?: string;  // Колір іконки
-  className?: string; // Додаткові CSS класи
+  iconId: string;  
+  width?: number;  
+  height?: number; 
+  color?: string;  
+  className?: string; 
 }
 
 const Icon: React.FC<IconProps> = ({ iconId, width = 24, height = 24, color = 'currentColor', className = '' }) => (
   <svg width={width} height={height} fill={color} className={className} aria-hidden="true">
-    <use xlinkHref={`#${iconId}`} />
+    <use href={`${icons}#${iconId}`} />           
   </svg>
 );
 
