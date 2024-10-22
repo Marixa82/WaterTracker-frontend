5b3a4efcef85cc90fe1eb3646e6b40eef263d299
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-22 18:02:23 +0300

    upd  styles TrackerBenefits

--- src/components/Welcome/TrackerBenefits.tsx
+++ src/components/Welcome/TrackerBenefits.tsx
@@ -1,17 +1,25 @@
+import { Container } from "react-bootstrap";
 import Icon from "../Icon";
+
+
 
 const TrackerBenefits: React.FC = () =>{
   return(
-    <div>
-      <ul>
-        <li>
-        <Icon iconId="icon-eye" width={32} height={32} color="black" />
-        <h3>Track your habits</h3>
-        </li>
-        <li><Icon iconId="icon-presentation-chart-bar" width={32} height={32} /><h3>View statistics</h3></li>
-        {/* <li><Icon iconId="icon-wrench" width={32} height={32} /><h3>Personal rate setting</h3></li> */}
-      </ul>
-    </div>
+
+    <>
+      <div className="card-border-0" >
+        
+          <ul className="ps-0">
+            <li className="d-flex align-items-center mb-3">
+              <Icon iconId="icon-calendar-days" width={32} height={32} color="inherit" />
+            <h3 className="ms-2 mb-0">Track your habits</h3>
+            </li>
+            <li className="d-flex align-items-center mb-3"><Icon iconId="icon-presentation-chart-bar" width={32} height={32} /><h3 className="ms-2 mb-0">View statistics</h3></li>
+            <li className="d-flex align-items-center"><Icon iconId="icon-eye-slash" width={32} height={32} /><h3 className="ms-2 mb-0">Personal rate setting</h3></li>
+          </ul>
+      </div>
+      <button type="button" className="btn btn-primary btn-lg w-100">Try tracker</button>
+    </>
   )
 }
 export default TrackerBenefits;
