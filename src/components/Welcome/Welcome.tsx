a2b07af3d6e267e257a6b920f6a15a78e84c1ef9
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-24 17:43:32 +0300

    upd component Welcome

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,15 +1,23 @@
 // import  { ReactNode } from 'react';
+
+// import { Container } from "react-bootstrap";
+
 // import TrackerBenefitsList from "./TrackerBenefitsList";
 interface WelcomeProps {
   trackerBenefitsList: JSX.Element;
+  whyDrinkWater: JSX.Element;
 }
 
-function Welcome({ trackerBenefitsList }: WelcomeProps) {
+function Welcome({ trackerBenefitsList, whyDrinkWater }: WelcomeProps) {
   return (
-    <>
-      <h1>WaterСonsumptionTracker</h1>
-      <h2>Record daily water intake and track</h2>
-      {trackerBenefitsList}
+    < >
+      <div className="d-flex m-0 flex-column container-sm">
+        <h1>WaterСonsumptionTracker</h1>
+        <h2>Record daily water intake and track</h2>
+        {trackerBenefitsList}
+        {whyDrinkWater}
+
+      </div>
     </>
   );
 }
