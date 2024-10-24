1a7f449889da8fb62e6e46f1b51425a9ab8be992
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-24 17:46:20 +0300

    upd TrackerBenefits

--- src/components/Welcome/TrackerBenefits.tsx
+++ src/components/Welcome/TrackerBenefits.tsx
@@ -1,3 +1,4 @@
+// import { Container } from "react-bootstrap";
 import Icon from "../Icon";
 
 
@@ -5,10 +6,10 @@ import Icon from "../Icon";
 const TrackerBenefits: React.FC = () =>{
   return(
 
-    <>
+    <div className=" d-flex flex-wrap container-sm p-0">
       <div className="card-border-0" >
-        
-          <ul className="ps-0">
+      <h3 className='mb-2,5'>TrackerBenefitsList</h3>
+          <ul className="p-0">
             <li className="d-flex align-items-center mb-3">
               <Icon iconId="icon-calendar-days" width={32} height={32} color="inherit" />
             <h3 className="ms-2 mb-0">Track your habits</h3>
@@ -18,7 +19,7 @@ const TrackerBenefits: React.FC = () =>{
           </ul>
       </div>
       <button type="button" className="btn btn-primary btn-lg w-100">Try tracker</button>
-    </>
+    </div>
   )
 }
 export default TrackerBenefits;
