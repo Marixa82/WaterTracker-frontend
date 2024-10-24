07bd4631255b36e9c0d448158956325a7c94bc2e
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-24 17:42:53 +0300

    upd Welcome page

--- src/pages/Welcome/WelcomePage.tsx
+++ src/pages/Welcome/WelcomePage.tsx
@@ -1,10 +1,11 @@
 import TrackerBenefitsList from "../../components/Welcome/TrackerBenefitsList";
 import Welcome from "../../components/Welcome/Welcome";
+import WhyDrinkWater from "../../components/Welcome/WhyDrinkWater";
 
 const WelcomePage = () =>{
     return(
         <div>
-      <Welcome trackerBenefitsList={<TrackerBenefitsList/>} />
+      <Welcome trackerBenefitsList={<TrackerBenefitsList/>}  whyDrinkWater={<WhyDrinkWater/>}/>
        
       
     </div>
