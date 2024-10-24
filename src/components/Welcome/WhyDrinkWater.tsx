2becf603b0cf3392e0b74b49864533fcb1c7734c
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-24 17:42:23 +0300

    create new component

---
+++ src/components/Welcome/WhyDrinkWater.tsx
@@ -0,0 +1,29 @@
+// import React, { ReactNode } from 'react';
+
+
+
+const WhyDrinkWater: React.FC = () =>{
+  return (
+    <div className="container-sm p-0 justify-content-sm-start">
+      <h3>WhyDrinkWater</h3>
+      <ul className="p-0">
+      <li>
+        <p>Supply of nutrients to all organs</p>
+      </li>
+      <li>
+        <p>Providing oxygen to the lungs</p>
+      </li>
+      <li>
+        <p>Maintaining the work of the heart</p>
+      </li>
+      <li><p>Release of processed substances</p></li>
+      <li>
+        <p>Ensuring the stability of the internal environment</p>
+      </li>
+      <li><p>Maintaining within the normal temperature</p></li>
+      <li><p>Maintaining an immune system capable of resisting disease</p></li></ul>
+    </div>
+  );
+}
+
+export default WhyDrinkWater;
