e103d36abc2bba5e7940b13f3fc7c29982a0073c
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-25 22:42:43 +0300

    upd WhyDrinkWater

--- src/components/Welcome/WhyDrinkWater.tsx
+++ src/components/Welcome/WhyDrinkWater.tsx
@@ -4,25 +4,27 @@
 
 const WhyDrinkWater: React.FC = () =>{
   return (
-    <div className="container-sm p-0 justify-content-sm-start">
-      <h3>WhyDrinkWater</h3>
-      <ul className="p-0">
-      <li>
-        <p>Supply of nutrients to all organs</p>
-      </li>
-      <li>
-        <p>Providing oxygen to the lungs</p>
-      </li>
-      <li>
-        <p>Maintaining the work of the heart</p>
-      </li>
-      <li><p>Release of processed substances</p></li>
-      <li>
-        <p>Ensuring the stability of the internal environment</p>
-      </li>
-      <li><p>Maintaining within the normal temperature</p></li>
-      <li><p>Maintaining an immune system capable of resisting disease</p></li></ul>
-    </div>
+    <div className="d-flex p-0 ">
+      <div >
+        <h3>WhyDrinkWater</h3>
+        <ul className="list-group list-group-flush">
+        <li className="list-group-item  p-0 border-0">
+          <p>Supply of nutrients to all organs</p>
+        </li>
+        <li>
+          <p>Providing oxygen to the lungs</p>
+        </li>
+        <li>
+          <p>Maintaining the work of the heart</p>
+        </li>
+        <li><p>Release of processed substances</p></li>
+        <li>
+          <p>Ensuring the stability of the internal environment</p>
+        </li>
+        <li><p>Maintaining within the normal temperature</p></li>
+        <li><p>Maintaining an immune system capable of resisting disease</p></li></ul>
+      </div>
+      </div>
   );
 }
 
