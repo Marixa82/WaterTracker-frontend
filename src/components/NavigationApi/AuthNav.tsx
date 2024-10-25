8d73cadf59d5c0201a5becdf748896b3810e5f77
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-25 22:41:20 +0300

    create AuthNav

---
+++ src/components/NavigationApi/AuthNav.tsx
@@ -0,0 +1,13 @@
+
+
+import { LOGIN_ROUTE, REGISTER_ROUTE } from "../constants/routes";
+import { NavLink } from "react-router-dom";
+
+export const AuthNav: React.FC = () =>{
+   return(
+    <div>
+<NavLink to={LOGIN_ROUTE}>Login</NavLink>
+<NavLink to={REGISTER_ROUTE}>Login</NavLink>
+    </div>
+   )
+};
