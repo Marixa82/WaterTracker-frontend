5c3a276397ceec455190189c9c6285ab901dcec4
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-26 14:47:53 +0300

    create Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,16 @@
+// import React, { ReactNode } from 'react';
+
+// interface LoaderProps {
+//   children: ReactNode;
+// }
+
+const Loader:React.FC = () => {
+  return (
+    <div className="d-flex align-items-center">
+  <strong>Loading...</strong>
+  <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+</div>
+  );
+}
+
+export default Loader;
