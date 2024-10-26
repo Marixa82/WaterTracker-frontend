86e587e1512f49fd5f3689016ea6f593ba75d850
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-26 15:43:00 +0300

    upd Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,28 @@
-import  { ReactNode } from 'react';
+
+
 
 interface HeaderProps {
-  children: ReactNode;
+  logoUrl: string;         
+  registrationUrl: string; 
 }
 
-function Header({ children }: HeaderProps) {
+function Header({ logoUrl, registrationUrl }: HeaderProps) {
   return (
     <>
-      <h1>Header</h1>
-      {children}
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src={logoUrl} alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+            Мій сайт
+          </a>
+          <div className="d-flex">
+            <a href={registrationUrl} className="navbar-brand" href="/signin">Реєстрація</a>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
 
 export default Header;
+
