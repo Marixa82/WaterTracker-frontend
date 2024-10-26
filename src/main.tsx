7cb162c0f5870d1d3ad7674fe4e15961b22e3c53
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-26 14:48:41 +0300

    upd main

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-// import './index.css'
+import './index.css'
 import { Provider } from 'react-redux';
-import { store } from './redux/store/index.ts'
+import { persistor, store } from './redux/store/index.ts'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename='/WaterTracker-frontend'>
+      <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 )
