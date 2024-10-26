704726922dfe9d6beafb5b182443d83307c2e617
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-26 14:50:55 +0300

    upd SignInPage

--- src/pages/SignIn/SignInPage.tsx
+++ src/pages/SignIn/SignInPage.tsx
@@ -1,9 +1,10 @@
-import BSIn from '../assets/Bottle/Bottle_for_SignIn.png'
+import SignForm from '../../components/SignIn/SignIn';
+// import BSIn from '../assets/Bottle/Bottle_for_SignIn.png'
 const SignInPage = () => {
   return (
     <>
       <h1>SignInPage</h1>
-      <img src={BSIn} alt="Bottle_for_SignIn" width={100} height={100} />
+      <SignForm/>
     </>
   );
 }
