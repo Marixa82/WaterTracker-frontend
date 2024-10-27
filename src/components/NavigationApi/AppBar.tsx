3fd9f7bc3002a7e9ff51b332f954ee698856c8d3
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-27 19:13:00 +0200

    upd AppBar.tsx

--- src/components/NavigationApi/AppBar.tsx
+++ src/components/NavigationApi/AppBar.tsx
@@ -2,11 +2,14 @@ import { UserMenu } from "../UserMenu/UserMenu";
 import { AuthNav } from "./AuthNav";
 import { Navigation } from "./Navigation";
 import { useAuth } from "../../hooks/useNavigationHook";
+// import Header from "../Header/Header";
+import Logo from "../Logo";
 
 export const AppBar = () => {
     const { isLoggedIn } = useAuth();
     return (
         <div >
+            <Logo/>
             <Navigation />
             {isLoggedIn ? <UserMenu /> : <AuthNav />}
         </div >
