dc77e3a6cb54e2d7b759ac57ba47e83f7a720eeb
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-10-29 23:20:20 +0200

    upd AuthNav

--- src/components/NavigationApi/AuthNav.tsx
+++ src/components/NavigationApi/AuthNav.tsx
@@ -1,13 +1,13 @@
+import Icon from "../Icon";
 
-
-import { LOGIN_ROUTE, REGISTER_ROUTE } from "../constants/routes";
+import { LOGIN_ROUTE} from "../constants/routes";
 import { NavLink } from "react-router-dom";
 
 export const AuthNav: React.FC = () =>{
    return(
     <div>
-<NavLink to={LOGIN_ROUTE}>Login</NavLink>
-<NavLink to={REGISTER_ROUTE}>Register</NavLink>
+<NavLink to={LOGIN_ROUTE}>Sign in <Icon iconId="icon-user" color="white"/></NavLink>
+{/* <NavLink to={REGISTER_ROUTE}>Register</NavLink> */}
     </div>
    )
 };
