9437bf51db373b7dc43039ae67cf66e30dd8e170
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-04 19:28:01 +0200

    upd rootReduser

--- src/redux/root/rootReduser.ts
+++ src/redux/root/rootReduser.ts
@@ -1,8 +1,23 @@
 import { combineReducers } from "redux";
 import { authReducer } from "../auth";
+import { waterReducer } from "../water";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+// Налаштування конфігурації для auth
+const authPersistConfig = {
+    key: "auth",
+    storage,
+    whitelist: ["token"],
+};
+
+// Створення rootReducer з використанням persistReducer для auth
 const rootReducer = combineReducers({
-    auth: authReducer,
-  });
-  
-  export default rootReducer;
+    auth: persistReducer(authPersistConfig, authReducer), // persist лише для authReducer
+    water: waterReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export default rootReducer;
+
+
