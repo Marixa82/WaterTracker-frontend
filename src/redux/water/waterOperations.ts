8e8b7120a786ef404baa2850f25fae3ebfcc5d67
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-07 22:14:33 +0200

    upd waterOperations

--- src/redux/water/waterOperations.ts
+++ src/redux/water/waterOperations.ts
@@ -1,13 +1,13 @@
 import { handleAsyncError, instance } from "../Api/api";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import {  UserWaterResponse, UserToken } from "../types";
+import {  UserWaterMonthResponse,UserWaterTodayResponse, UserToken } from "../types";
 
-const fetchWaterData = createAsyncThunk<UserWaterResponse, UserToken,{rejectValue: string}>(
-    "waterData/fetchAll",
+const getWaterMonthData = createAsyncThunk<UserWaterMonthResponse, UserToken,{rejectValue: string}>(
+    "waterMonthData/fetchAll",
     async (_, thunkAPI) => {
         try {
             // token.set(userToken.token);
-            const response = await instance.get(`waterData`);
+            const response = await instance.get(`waterMonthData`);
             return response.data;
             
         } catch (error) {
@@ -15,8 +15,21 @@ const fetchWaterData = createAsyncThunk<UserWaterResponse, UserToken,{rejectValu
         }
     }
 );
-const changeWaterData = createAsyncThunk<UserWaterResponse,UserWaterResponse ,{rejectValue: string}>(
-    "waterData/changeWaterData",
+const getWaterTodayData = createAsyncThunk<UserWaterTodayResponse[], UserToken,{rejectValue: string}>(
+    "waterTodayData/fetchAll",
+    async (_, thunkAPI) => {
+        try {
+            
+            const response = await instance.get(`waterTodayData`);
+            return response.data;
+            
+        } catch (error) {
+            return handleAsyncError(error, thunkAPI);
+        }
+    }
+);
+const addWaterData = createAsyncThunk<UserWaterTodayResponse ,{rejectValue: string}>(
+    "waterData/addWaterData",
     async (items, thunkAPI) => {
         try {
             const response = await instance.post("waterData", items);
@@ -26,60 +39,29 @@ const changeWaterData = createAsyncThunk<UserWaterResponse,UserWaterResponse ,{r
         }
     }
 );
-//Registration user//
-// const signUp = createAsyncThunk<SignInResponse,Credentials,{rejectValue: string;}>('auth/singUp', async (credentials, thunkAPI) => {
-//     try {
-//         const { data } = await instance.post(`/users/signUp`, credentials);
-//         token.set(data.token);
-//         console.log('credentials', data)
-//         return data;
-//     } catch (error) {
-//         return handleAsyncError(error, thunkAPI);
-        
-// }});
-
-// //LoginIn user//
-// const signIn = createAsyncThunk<SignInResponse,Credentials,{rejectValue: string;}>('auth/signIn', async (credentials, thunkAPI) => {
-    
-//         const response = await instance.post(`/users/signIn`, credentials);
-//         const {data} = response;
-//         token.set(data.token);
-//         if (!data) {
-//         return thunkAPI.rejectWithValue('message'); }
-//         return data;
-//     }
-// );
-
-// //LogOut user//
-// const logOut = createAsyncThunk('auth/logOut', async (_, thunkAPI) => {
-//     try {
-//         await instance.post(`/users/logOut`);
-//         token.unset();
-//     } catch (error) {
-//         return handleAsyncError(error, thunkAPI);
-        
-// }
-// });
-
-// //Back user from LocalStorage - refresh//
-
-// const fetchCurrentUser = createAsyncThunk<CurrentUser,void,{rejectValue: string;}>('auth/refresh', async (_, thunkAPI) => {
-//     const state = thunkAPI.getState()as { auth: { token: string | null }} ;
-//     const persistedToken = state.auth.token;
-
-//     if (!persistedToken) {
-//         return thunkAPI.rejectWithValue('Please register...');
-//     }
-//     token.set(persistedToken);
-//     try {
-//         const { data } = await instance.get('/users/current');
-//         return data;
-//     } catch (error) {
-//         return handleAsyncError(error, thunkAPI);
-        
-// }
-// })
+const editWaterData = createAsyncThunk<UserWaterTodayResponse,UserWaterTodayResponse ,{rejectValue: string}>(
+    "waterData/editWaterData",
+    async (items, thunkAPI) => {
+        try {
+            const response = await instance.post("waterData", items);
+            return response.data;
+        } catch (error) {
+            return handleAsyncError(error, thunkAPI);
+        }
+    }
+);
+const deleteWaterData = createAsyncThunk< string, string,{rejectValue: string}>(
+    "waterData/deleteWaterItem",
+    async (waterId, thunkAPI) => {
+        try {
+            const response = await instance.delete(`waterData/${waterId}`);
+            return response.data;
+        } catch (error) {
+            return handleAsyncError(error, thunkAPI);
+        }
+    }
+);
 
 
-const waterOperations = {  fetchWaterData, changeWaterData };
+const waterOperations = {  getWaterMonthData, getWaterTodayData,editWaterData, addWaterData, deleteWaterData };
 export default waterOperations;
