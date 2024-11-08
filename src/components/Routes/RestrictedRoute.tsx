14ed7e099dedef94b7d49305d023554e4d1a036b
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-08 20:00:16 +0200

    upd RestrictedRoute

--- src/components/Routes/RestrictedRoute.tsx
+++ src/components/Routes/RestrictedRoute.tsx
@@ -1,17 +1,28 @@
 // import { HOME_ROUTE } from '../constants/routes';
 import  { ReactNode } from 'react';
-import useAuthRedirect from "../../hooks/useNavigationHook";
+import { useAppSelector } from '../../hooks/hook';
+import { authSelectors } from '../../redux/auth';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+// import useAuthRedirect from "../../hooks/useNavigationHook";
 
 
 interface RestrictedRouteProps {
     children: ReactNode;
-    redirectTo?: string;
+    redirectTo: string;
   }
   
-  const RestrictedRoute = ({ children }: RestrictedRouteProps) => {
-    const authenticated = useAuthRedirect();
-
-    return authenticated ? <>{children}</> : null;
+  const RestrictedRoute: React.FC<RestrictedRouteProps> = ({ children, redirectTo }) => {
+    const isLoggedIn = useAppSelector(authSelectors.selectUserIsLoggedIn);
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      // Якщо користувач авторизований, то редиректимо його на redirectTo
+      if (isLoggedIn) {
+        navigate(redirectTo, { replace: true });
+      }
+    }, [isLoggedIn, navigate, redirectTo]);
+    return !isLoggedIn ? <>{children}</> : null;
   };
   
   export default RestrictedRoute;
