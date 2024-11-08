b2c5ce1aa29d929ecf75c6a25a38c8d8f2dcdabf
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-08 19:56:06 +0200

    upd AppRoutes

--- src/components/constants/appRoutes.tsx
+++ src/components/constants/appRoutes.tsx
@@ -1,9 +1,8 @@
 import { ReactNode } from "react";
 import PrivateRoute from "../Routes/PrivateRoute";
 import RestrictedRoute from "../Routes/RestrictedRoute";
-import favouriteBooks from "../FavouriteBooks";
-import { HOME_ROUTE,WATER_ROUTE,LOGIN_ROUTE,REGISTER_ROUTE, SignInPage, SignUpPage, BookListPage } from "./routes";
-import WelcomePage from "../../pages/Welcome/WelcomePage";
+import { HOME_ROUTE,LOGIN_ROUTE,REGISTER_ROUTE, WELCOME_ROUTE, SignUpPage, WelcomePage, HomePage } from "./routes";
+import SignInPage from "../../pages/SignIn/SignInPage";
 
 
 
@@ -17,29 +16,28 @@ interface Route {
 // Масив маршрутів
 export const appRoutes: Route[] = [
     {
-        path: HOME_ROUTE,
+        path: WELCOME_ROUTE,
         element: <WelcomePage />, 
     },
     {
-        path: WATER_ROUTE,
+        path: HOME_ROUTE,
         element: (
             <PrivateRoute>
-                <BookListPage books={favouriteBooks}/>
+                <HomePage />
             </PrivateRoute>
         ),
     },
     {
         path: LOGIN_ROUTE,
-        element: (
-            <RestrictedRoute redirectTo={WATER_ROUTE}>
-                <SignInPage />
+        element:(<RestrictedRoute redirectTo={HOME_ROUTE}><SignInPage/>
             </RestrictedRoute>
+          
         ),
     },
     {
         path: REGISTER_ROUTE,
         element: (
-            <RestrictedRoute redirectTo={WATER_ROUTE}>
+            <RestrictedRoute redirectTo={HOME_ROUTE}>
                 <SignUpPage />
             </RestrictedRoute>
         ),
