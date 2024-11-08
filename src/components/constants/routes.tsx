bb159a42edc0f06f1b1f906fd357fb8b76241a28
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-08 19:56:22 +0200

    upd routes

--- src/components/constants/routes.tsx
+++ src/components/constants/routes.tsx
@@ -1,14 +1,14 @@
 // import path from "path";
 
 import { lazy } from 'react';
-export const HOME_ROUTE: string = '/'; 
+export const WELCOME_ROUTE: string = '/'; 
 export const LOGIN_ROUTE: string = '/login';
 export const REGISTER_ROUTE: string = '/register';
-export const WATER_ROUTE: string = '/water';
+export const HOME_ROUTE: string = '/home';
 
 const HomePage = lazy(() => import('../../pages/Home/home-page'));
-const SignInPage = lazy(() => import('../../pages/SignIn/SignInPage'));
+// const SignInPage = lazy(() => import('../../pages/SignIn/SignInPage'));
 const SignUpPage = lazy(() => import('../../pages/SignUp/SignUpPage'));
-const BookListPage = lazy(() => import('../BookList'));
+const WelcomePage = lazy(() => import('../../pages/Welcome/WelcomePage'));
 
-export {HomePage, SignInPage, SignUpPage, BookListPage} 
+export {HomePage, SignUpPage, WelcomePage} 
