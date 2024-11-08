692914ceea57e449015a73def1bcd627fd7f3ec7
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-08 19:59:59 +0200

    upd PrivateRoute

--- src/components/Routes/PrivateRoute.tsx
+++ src/components/Routes/PrivateRoute.tsx
@@ -1,17 +1,19 @@
-// import { LOGIN_ROUTE } from '../constants/routes';
 import  { ReactNode } from 'react';
-import useAuthRedirect from "../../hooks/useNavigationHook";
+import { useAppSelector } from '../../hooks/hook';
+import { authSelectors } from '../../redux/auth';
+import { LOGIN_ROUTE } from '../constants/routes';
+import { Navigate } from 'react-router-dom';
+
 
 
 interface PrivateRouteProps {
     children: ReactNode;
     redirectTo?: string;
   }
-  
-  const PrivateRoute = ({ children }: PrivateRouteProps) => {
-    const authenticated = useAuthRedirect();
 
-  return authenticated ? <>{children}</> : null;
+  const PrivateRoute: React.FC<PrivateRouteProps> = ({ children, redirectTo = LOGIN_ROUTE }) => {
+    const authenticated = useAppSelector(authSelectors.selectUserIsLoggedIn)
+  return authenticated ? <>{children}</> : <Navigate to={redirectTo} replace />;
   };
   
   export default PrivateRoute;
