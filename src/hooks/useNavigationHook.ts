b87a40d6e37c6b6e4c64f2667a3aeefff6bd1b9e
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-08 20:01:37 +0200

    upd useNavHook

--- src/hooks/useNavigationHook.ts
+++ src/hooks/useNavigationHook.ts
@@ -1,22 +1,20 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../hooks/hook";
 import { authSelectors } from "../redux/auth";
 
-const useAuthRedirect = (redirectTo: string = "/login") => {
-  const authenticated = useAppSelector(authSelectors.selectUserIsLoggedIn);
-  const navigate = useNavigate();
+// const useAuthRedirect = (redirectTo: string = "/login") => {
+//   const authenticated = useAppSelector(authSelectors.selectUserIsLoggedIn);
+//   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!authenticated) {
-      navigate(redirectTo, { replace: true });
-    }
-  }, [authenticated, navigate, redirectTo]);
+//   useEffect(() => {
+//     if (!authenticated) {
+//       navigate(redirectTo, { replace: true });
+//     }
+//   }, [authenticated, navigate, redirectTo]);
 
-  return authenticated;
-};
+//   return authenticated;
+// };
 
-export default useAuthRedirect;
+// export default useAuthRedirect;
 
 export const useAuth = () => {
   const isLoggedIn = useAppSelector(authSelectors.selectUserIsLoggedIn);
