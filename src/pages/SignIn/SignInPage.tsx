e99b0a1d5a304540125ff80e649672a47a137741
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-08 20:02:07 +0200

    upd SignInPage

--- src/pages/SignIn/SignInPage.tsx
+++ src/pages/SignIn/SignInPage.tsx
@@ -1,10 +1,13 @@
-import SignForm from '../../components/SignIn/SignIn';
+import SignForm from "../../components/SignIn/SignIn";
+// import SignUp from "../../components/SignUp/SignUp";
+
 // import BSIn from '../assets/Bottle/Bottle_for_SignIn.png'
-const SignInPage = () => {
+const SignInPage: React.FC = () =>  {
   return (
     <>
       <h1>SignInPage</h1>
-      <SignForm/>
+      
+    <SignForm/>
     </>
   );
 }
