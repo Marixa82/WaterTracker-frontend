bee93406c123a48b97875add705c2d030a92563f
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-10 22:35:43 +0200

    upd UserLogoModal

--- src/components/PopUp/UserLogoModal.tsx
+++ src/components/PopUp/UserLogoModal.tsx
@@ -1,25 +1,52 @@
 // import React, { ReactNode } from 'react';
 
 import Icon from "../Icon";
+interface UserLogoModalProps {
+  onClose: () => void;
+}
 
-
-
-function UserLogoModal() {
+const UserLogoModal: React.FC<UserLogoModalProps> = ({ onClose }) => {
   return (
-    <div className="modal">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <h1 hidden>UserLogoModal</h1>
-          <div className="modal-body">
-            <ul className="modal-menu" aria-labelledby="dropdownMenuButton1">
-              <li><button className="menu-item"><Icon iconId="icon-cog-6-tooth"/>Setting</button></li>
-              <li><button className="menu-item"><Icon iconId="icon-arrow-right-on-rectangle"/>Log out</button></li>
-            </ul>
-          </div>
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button onClick={onClose} className="close-button">×</button>
+        <h2 className="visually-hidden">User Logo Modal</h2>
+        <div className="modal-body">
+          <ul className="modal-menu" aria-labelledby="dropdownMenuButton1">
+            <li>
+              <button className="menu-item">
+                <Icon iconId="icon-cog-6-tooth" />
+                <span>Settings</span>
+              </button>
+            </li>
+            <li>
+              <button className="menu-item">
+                <Icon iconId="icon-arrow-right-on-rectangle" />
+                <span>Log out</span>
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
   );
 }
+// const UserLogoModal: React.FC<UserLogoModalProps> = ({ onClose }) =>{
+//   return (
+//     <div className="modal">
+//       <div className="modal-content">
+//       <button onClick={onClose} className="close-button">×</button>
+//           <h2 className="visually-hidden">UserLogoModal</h2>
+//           <div className="modal-body">
+//             <ul className="modal-menu" aria-labelledby="dropdownMenuButton1">
+//               <li><button className="menu-item"><Icon iconId="icon-cog-6-tooth"/>Setting</button></li>
+//               <li><button className="menu-item"><Icon iconId="icon-arrow-right-on-rectangle"/>Log out</button></li>
+//             </ul>
+//           </div>
+//       </div>
+      
+//     </div>
+//   );
+// }
 
 export default UserLogoModal;
