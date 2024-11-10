6a6034fc664418f7f469a652a93a260aab7a97ca
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-10 22:35:05 +0200

    upd AuthNav

--- src/components/NavigationApi/AuthNav.tsx
+++ src/components/NavigationApi/AuthNav.tsx
@@ -1,15 +1,23 @@
 import Icon from "../Icon";
-
+import UserLogo from "../PopUp/UserLogo";
 import { LOGIN_ROUTE} from "../constants/routes";
 import { NavLink } from "react-router-dom";
 
-export const AuthNav: React.FC = () =>{
-   return(
+interface AuthNavProps {
+  isLoggedIn: boolean;
+}
+
+export const AuthNav: React.FC<AuthNavProps> = ({ isLoggedIn }) => {
+  return (
     <div>
-      <NavLink to={LOGIN_ROUTE} className="auth-nav-link">
-        <span>Sign in</span>
-        <Icon iconId="icon-user"  />
-      </NavLink>
+      {!isLoggedIn ? (
+        <NavLink to={LOGIN_ROUTE} className="auth-nav-link">
+          <span>Sign in</span>
+          <Icon iconId="icon-user" />
+        </NavLink>
+      ) : (
+        <UserLogo/>
+      )}
     </div>
-   )
+  );
 };
