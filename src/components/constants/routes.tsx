70457d0ebbacc1c0a308cf2e7b3df8383e87baa0
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-10 22:34:13 +0200

    upd routes

--- src/components/constants/routes.tsx
+++ src/components/constants/routes.tsx
@@ -7,8 +7,8 @@ export const REGISTER_ROUTE: string = '/register';
 export const HOME_ROUTE: string = '/home';
 
 const HomePage = lazy(() => import('../../pages/Home/home-page'));
-// const SignInPage = lazy(() => import('../../pages/SignIn/SignInPage'));
+const SignInPage = lazy(() => import('../../pages/SignIn/SignInPage'));
 const SignUpPage = lazy(() => import('../../pages/SignUp/SignUpPage'));
 const WelcomePage = lazy(() => import('../../pages/Welcome/WelcomePage'));
 
-export {HomePage, SignUpPage, WelcomePage} 
+export {HomePage, SignUpPage,SignInPage, WelcomePage} 
