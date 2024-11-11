9b8dd23cbf83056ccf76c673db6ef946e3bf684b
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-11 19:42:46 +0200

    upd OpenButtonModals

--- src/components/PopUp/OpenButtonModals.tsx
+++ src/components/PopUp/OpenButtonModals.tsx
@@ -1,4 +1,6 @@
 import Icon from "../Icon";
+import { useAppDispatch } from "../../hooks/hook";
+import { authOperations } from "../../redux/auth";
 
 interface OpenSettingButtonProps {
     onClick: () => void;
@@ -24,3 +26,47 @@ export const LogoutButton: React.FC<LogoutButtonProps> = ({onClick}) => {
                 </button>
     )
 }
+
+interface UploadUserPhotoButtonProps {
+  onUpload: (newAvatarUrl: string) => void;
+}
+
+export const UploadUserPhotoButton: React.FC<UploadUserPhotoButtonProps> = ({ onUpload }) => {
+  const dispatch = useAppDispatch();
+
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      // Відправляємо запит на бекенд для завантаження фото
+      const resultAction = await dispatch(authOperations.uploadAvatarUser({ file }));
+      if (authOperations.uploadAvatarUser.fulfilled.match(resultAction)) {
+        const newAvatarUrl = resultAction.payload.avatarURL; // Отримуємо URL з результату запиту
+        onUpload(newAvatarUrl); // Викликаємо onUpload з URL нового аватара
+      }
+    }
+  };
+
+  return (
+    <label className="menu-item">
+      <Icon iconId="icon-arrow-up-tray" />
+      <span>Upload Photo</span>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+        style={{ display: 'none' }} // прихований інпут
+      />
+    </label>
+  );
+};
+// interface UploadUserPhotoButtonProps {
+//   onClick: () => void;
+// }
+// export const UploadUserPhotoButton: React.FC<UploadUserPhotoButtonProps> = ({onClick}) => {
+//   return (
+//     <button className="menu-item" onClick={onClick}>
+//                   <Icon iconId="icon-arrow-up-tray" />
+//                   <span>Upload Photo</span>
+//                 </button>
+//   )
+// }
