a2bd7bfaf5b9923083ddfc7035bca24eef0b6874
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-11 17:49:21 +0200

    upd Setting

--- src/components/PopUp/Setting.tsx
+++ src/components/PopUp/Setting.tsx
@@ -1,16 +1,32 @@
-import { ReactNode } from 'react';
-
 interface SettingProps {
-  children: ReactNode;
+  onClose: () => void;
 }
 
-function Setting({ children }: SettingProps) {
+export const Setting: React.FC<SettingProps> = ({ onClose }) =>{
   return (
-    <>
-      <h1>Setting</h1>
-      {children}
-    </>
+    <div className="modal-overlay" onClick={onClose}>
+    <div className="modal-dialog modal-dialog-centered" onClick={(e) => e.stopPropagation()}>
+      <div className="modal-content">
+        <div className="modal-header">
+          <h1 className="modal-title fs-5" id="exampleModalLabel">
+            Setting
+          </h1>
+          <button type="button" className="btn-close" onClick={onClose} aria-label="Close"></button>
+        </div>
+          <div className="modal-body">
+            {/* Тут ви можете додати контент модального вікна */}
+            Налаштування користувача
+          </div>
+          <div className="modal-footer">
+            
+            <button type="button" className="btn btn-primary">
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
-export default Setting;
+ 
