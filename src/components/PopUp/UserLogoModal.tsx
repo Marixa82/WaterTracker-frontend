457ee4c54dfe30bbb5f9903f3d3be666582ceaa0
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-11 17:49:58 +0200

    upd UserLogoModal

--- src/components/PopUp/UserLogoModal.tsx
+++ src/components/PopUp/UserLogoModal.tsx
@@ -1,52 +1,69 @@
-// import React, { ReactNode } from 'react';
+import { useState } from "react";
+import {Setting} from "./Setting";
+import { LogOutModal } from "./LogOut";
+import { LogoutButton, OpenSettingButton } from "./OpenButtonModals";
+import {ModalWrapper} from "./ModalWrapper";
 
-import Icon from "../Icon";
 interface UserLogoModalProps {
   onClose: () => void;
 }
 
-const UserLogoModal: React.FC<UserLogoModalProps> = ({ onClose }) => {
+export const UserLogoModal: React.FC<UserLogoModalProps> = ({ onClose }) => {
+
+  const [isSettingOpen, setIsSettingOpen] = useState(false);
+  const [isLogoutOpen, setIsLogoutOpen] = useState(false);
+  const [isUserLogoModalOpen, setIsUserLogoModalOpen] = useState(true); 
+
+  const handleSettingOpen = () => {
+    setIsSettingOpen(true);
+    setIsUserLogoModalOpen(false);
+  };
+
+  const handleLogoutOpen = () => {
+    setIsLogoutOpen(true);
+    setIsUserLogoModalOpen(false);
+  };
+
+  const handleModalClose = () => {
+    setIsSettingOpen(false);
+    setIsLogoutOpen(false);
+    setIsUserLogoModalOpen(true);
+  };
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <button onClick={onClose} className="close-button">×</button>
-        <h2 className="visually-hidden">User Logo Modal</h2>
-        <div className="modal-body">
-          <ul className="modal-menu" aria-labelledby="dropdownMenuButton1">
-            <li>
-              <button className="menu-item">
-                <Icon iconId="icon-cog-6-tooth" />
-                <span>Settings</span>
-              </button>
-            </li>
-            <li>
-              <button className="menu-item">
-                <Icon iconId="icon-arrow-right-on-rectangle" />
-                <span>Log out</span>
-              </button>
-            </li>
-          </ul>
+    <>
+      {isUserLogoModalOpen && (
+        <div className="modal-overlay" onClick={onClose}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <button onClick={onClose} className="close-button visually-hidden">×</button>
+            <h2 className="visually-hidden">User Logo Modal</h2>
+            <div className="modal-body">
+              {/* Кнопки для відкриття модалок зникнуть, коли яка-небудь модалка відкрита */}
+              {!isSettingOpen && !isLogoutOpen && (
+                <ul className="modal-menu" aria-labelledby="dropdownMenuButton1">
+                  <li>
+                    <OpenSettingButton onClick={handleSettingOpen} />
+                  </li>
+                  <li>
+                    <LogoutButton onClick={handleLogoutOpen} />
+                  </li>
+                </ul>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+
+      {/* Відкриття модалок */}
+      <ModalWrapper isOpen={isSettingOpen} onClose={handleModalClose}>
+        <Setting onClose={handleModalClose} />
+      </ModalWrapper>
+
+      <ModalWrapper isOpen={isLogoutOpen} onClose={handleModalClose}>
+        <LogOutModal onClose={handleModalClose} />
+      </ModalWrapper>
+    </>
   );
 }
-// const UserLogoModal: React.FC<UserLogoModalProps> = ({ onClose }) =>{
-//   return (
-//     <div className="modal">
-//       <div className="modal-content">
-//       <button onClick={onClose} className="close-button">×</button>
-//           <h2 className="visually-hidden">UserLogoModal</h2>
-//           <div className="modal-body">
-//             <ul className="modal-menu" aria-labelledby="dropdownMenuButton1">
-//               <li><button className="menu-item"><Icon iconId="icon-cog-6-tooth"/>Setting</button></li>
-//               <li><button className="menu-item"><Icon iconId="icon-arrow-right-on-rectangle"/>Log out</button></li>
-//             </ul>
-//           </div>
-//       </div>
-      
-//     </div>
-//   );
-// }
 
-export default UserLogoModal;
+
+
