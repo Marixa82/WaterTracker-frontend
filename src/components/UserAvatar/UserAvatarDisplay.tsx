8133d076f9dabed0708af3cb6a51117d8535be5c
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-12 17:50:14 +0200

    create UserAvatarDisplay

---
+++ src/components/UserAvatar/UserAvatarDisplay.tsx
@@ -0,0 +1,40 @@
+
+import { UploadUserPhotoButton } from '../PopUp/OpenButtonModals';
+
+interface UserAvatarDisplayProps {
+  avatarUrl?: string;
+  avatarText?: string;
+  onUploadComplete: (newAvatarUrl: string) => void;
+}
+
+const UserAvatarDisplay: React.FC<UserAvatarDisplayProps> = ({ avatarUrl,avatarText, onUploadComplete })=> {
+  return (
+    <div className="avatar-section d-flex justify-content-between align-items-center w-100">
+    {/* Ліва частина: Аватар */}
+    <div >
+      {avatarUrl ? (
+        <img
+          src={avatarUrl}
+          alt="avatar"
+          className="rounded-circle me-2"
+          style={{ width: '32px', height: '32px' }}
+        />
+      ) : (
+        <span
+          className="avatar-placeholder rounded-circle me-2 d-flex align-items-center justify-content-center"
+          style={{ width: '32px', height: '32px', backgroundColor: '#ccc' }}
+        >
+          {avatarText}
+        </span>
+      )}
+    </div>
+
+    {/* Права частина: Кнопка завантаження */}
+    <div >
+      <UploadUserPhotoButton onUpload={onUploadComplete} />
+    </div>
+  </div>
+  );
+}
+
+export default UserAvatarDisplay;
