159ff58fe87e74f6bfe88b6c92adddec42a69dcf
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-13 18:35:38 +0200

    upd setting

--- src/components/PopUp/Setting.tsx
+++ src/components/PopUp/Setting.tsx
@@ -1,4 +1,7 @@
-import { UploadUserPhotoButton } from "./OpenButtonModals";
+import { FormUserData } from "../Forms/FormUserData";
+import PasswordForm from "../Forms/PasswordForm";
+import UserAvatarDisplay from "../UserAvatar/UserAvatarDisplay";
+// import { UploadUserPhotoButton } from "./OpenButtonModals";
 import { useState } from "react";
 
 interface SettingProps {
@@ -8,13 +11,14 @@ interface SettingProps {
 
 export const Setting: React.FC<SettingProps> = ({ initialAvatarUrl, onClose }) =>{
   const [avatarUrl, setAvatarUrl] = useState(initialAvatarUrl);
+  
   const handleUploadComplete = (newAvatarUrl: string) => {
     // Оновлюємо стан з новою URL після успішного завантаження аватара
     setAvatarUrl(newAvatarUrl);
   };
   return (
-    <div className="modal-overlay" onClick={onClose}>
-    <div className="modal-dialog" onClick={(e) => e.stopPropagation()}>
+    <div className="modal show d-block" tabIndex={-1} onClick={onClose} style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+    <div className="modal-dialog modal-dialog-centered" onClick={(e) => e.stopPropagation()}>
       <div className="modal-content">
         <div className="modal-header d-flex justify-content-between align-items-center">
           <h1 className="modal-title fs-5" id="exampleModalLabel">
@@ -22,17 +26,19 @@ export const Setting: React.FC<SettingProps> = ({ initialAvatarUrl, onClose }) =
           </h1>
           <button type="button" className="btn-close" onClick={onClose} data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-          <div className="modal-body">
+          <div className="modal-body ">
             <h2 className="modal-title">Your photo</h2>
-            {avatarUrl ? (
-            <img src={avatarUrl} alt='avatar' className="rounded-circle me-2" style={{ width: '32px', height: '32px' }} />
-          ) : (
-            <span className="avatar-placeholder rounded-circle me-2 d-flex align-items-center justify-content-center" style={{ width: '32px', height: '32px', backgroundColor: '#ccc' }}>
-              {<UploadUserPhotoButton onUpload={handleUploadComplete}/>}
-            </span>
-          )}
-            {/* Тут ви можете додати контент модального вікна */}
-            Налаштування користувача
+            <div className="modal-section">
+             <UserAvatarDisplay onUploadComplete={handleUploadComplete} avatarUrl={avatarUrl}/>
+            </div>
+            <div className="row g-2">
+              <div className="col-xl-6 col-md-12">
+                <FormUserData/>
+                </div>
+                <div className="col-xl-6 col-md-12">
+                <PasswordForm/>
+                </div>
+                </div>
           </div>
           <div className="modal-footer">
             
@@ -46,4 +52,17 @@ export const Setting: React.FC<SettingProps> = ({ initialAvatarUrl, onClose }) =
   );
 };
 
- 
+{/* <form>
+<div className="mb-3">
+  <label htmlFor="outdatedPassword" className="form-label">Outdated password:</label>
+  <input type="password" className="form-control" id="outdatedPassword" placeholder="123qaz@456" />
+</div>
+<div className="mb-3">
+  <label htmlFor="additionalInfo" className="form-label">Additional Info</label>
+  <textarea className="form-control" id="additionalInfo" rows={3}></textarea>
+</div>
+<div className="mb-3">
+  <label htmlFor="additionalInfo" className="form-label">Additional Info</label>
+  <textarea className="form-control" id="additionalInfo" rows={3}></textarea>
+</div>
+</form> */}
