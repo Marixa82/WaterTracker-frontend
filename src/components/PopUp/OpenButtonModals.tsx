b86ce2e714c5ec1033b72c981acbff46e6e87ca9
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-17 23:35:26 +0200

    upd openButtonModals

--- src/components/PopUp/OpenButtonModals.tsx
+++ src/components/PopUp/OpenButtonModals.tsx
@@ -1,6 +1,7 @@
 import Icon from "../Icon";
 import { useAppDispatch } from "../../hooks/hook";
 import { authOperations } from "../../redux/auth";
+import { UploadPhotoContainer, AvatarWrapper, AvatarImage, AvatarInitial, UploadLabel, HiddenFileInput } from "./PopUp.styled";
 
 interface OpenSettingButtonProps {
     onClick: () => void;
@@ -20,53 +21,83 @@ interface LogoutButtonProps {
 }
 export const LogoutButton: React.FC<LogoutButtonProps> = ({onClick}) => {
     return (
-        <button className="menu-item" onClick={onClick}>
+        <button onClick={onClick}>
                   <Icon iconId="icon-arrow-right-on-rectangle" />
                   <span>Log out</span>
                 </button>
     )
 }
-
 interface UploadUserPhotoButtonProps {
-  onUpload: (newAvatarUrl: string) => void;
+  onUpload: (avatarUrl: string) => void;
+  currentAvatarUrl?: string; 
+  userName?: string; 
 }
 
-export const UploadUserPhotoButton: React.FC<UploadUserPhotoButtonProps> = ({ onUpload }) => {
+export const UploadUserPhotoButton: React.FC<UploadUserPhotoButtonProps> = ({ onUpload, currentAvatarUrl, userName }) => {
   const dispatch = useAppDispatch();
 
   const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
-      // Відправляємо запит на бекенд для завантаження фото
       const resultAction = await dispatch(authOperations.uploadAvatarUser({ file }));
       if (authOperations.uploadAvatarUser.fulfilled.match(resultAction)) {
-        const newAvatarUrl = resultAction.payload.avatarURL; // Отримуємо URL з результату запиту
-        onUpload(newAvatarUrl); // Викликаємо onUpload з URL нового аватара
+        const newAvatarUrl = resultAction.payload.avatarURL;
+        onUpload(newAvatarUrl);
       }
     }
   };
 
+  const renderAvatar = () => {
+    if (currentAvatarUrl) {
+      return <AvatarImage src={currentAvatarUrl} alt="User Avatar" />;
+    } else {
+      return <AvatarInitial>{userName?.charAt(0)?.toUpperCase()}</AvatarInitial>;
+    }
+  };
+
   return (
-    <label className="menu-item">
-      <Icon iconId="icon-arrow-up-tray" />
-      <span>Upload Photo</span>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleFileChange}
-        style={{ display: 'none' }} // прихований інпут
-      />
-    </label>
+    <UploadPhotoContainer>
+      <AvatarWrapper>
+        {renderAvatar()}
+      </AvatarWrapper>
+      <UploadLabel>
+        <Icon iconId="icon-arrow-up-tray" />
+        <span>Upload Photo</span>
+        <HiddenFileInput type="file" accept="image/*" onChange={handleFileChange} />
+      </UploadLabel>
+    </UploadPhotoContainer>
   );
 };
+
 // interface UploadUserPhotoButtonProps {
-//   onClick: () => void;
+//   onUpload: (newAvatarUrl: string) => void;
 // }
-// export const UploadUserPhotoButton: React.FC<UploadUserPhotoButtonProps> = ({onClick}) => {
+
+// export const UploadUserPhotoButton: React.FC<UploadUserPhotoButtonProps> = ({ onUpload }) => {
+//   const dispatch = useAppDispatch();
+
+//   const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const file = event.target.files?.[0];
+//     if (file) {
+//       // Відправляємо запит на бекенд для завантаження фото
+//       const resultAction = await dispatch(authOperations.uploadAvatarUser({ file }));
+//       if (authOperations.uploadAvatarUser.fulfilled.match(resultAction)) {
+//         const newAvatarUrl = resultAction.payload.avatarURL; // Отримуємо URL з результату запиту
+//         onUpload(newAvatarUrl); // Викликаємо onUpload з URL нового аватара
+//       }
+//     }
+//   };
+
 //   return (
-//     <button className="menu-item" onClick={onClick}>
-//                   <Icon iconId="icon-arrow-up-tray" />
-//                   <span>Upload Photo</span>
-//                 </button>
-//   )
-// }
+//     <label className="menu-item">
+//       <Icon iconId="icon-arrow-up-tray" />
+//       <span>Upload Photo</span>
+//       <input
+//         type="file"
+//         accept="image/*"
+//         onChange={handleFileChange}
+//         style={{ display: 'none' }} // прихований інпут
+//       />
+//     </label>
+//   );
+// };
