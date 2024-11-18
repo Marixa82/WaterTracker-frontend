3d70e1e06ba43d06220de559e6e351eb2284ee06
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-18 21:27:19 +0200

    upd WhyDrinkWater

--- src/components/Welcome/WhyDrinkWater.tsx
+++ src/components/Welcome/WhyDrinkWater.tsx
@@ -4,28 +4,34 @@
 
 const WhyDrinkWater: React.FC = () =>{
   return (
-    <div className="d-flex p-0 ">
-      <div >
-        <h3>WhyDrinkWater</h3>
-        <ul className="list-group list-group-flush">
-        <li className="list-group-item  p-0 border-0">
-          <p>Supply of nutrients to all organs</p>
-        </li>
-        <li>
-          <p>Providing oxygen to the lungs</p>
-        </li>
-        <li>
-          <p>Maintaining the work of the heart</p>
-        </li>
-        <li><p>Release of processed substances</p></li>
-        <li>
-          <p>Ensuring the stability of the internal environment</p>
-        </li>
-        <li><p>Maintaining within the normal temperature</p></li>
-        <li><p>Maintaining an immune system capable of resisting disease</p></li></ul>
+    <div className="container my-4">
+    <h3>Why Drink Water</h3>
+    <div className="list-container">
+      <div className="list-item">
+        <p>Supply of nutrients to all organs</p>
       </div>
+      <div className="list-item">
+        <p>Providing oxygen to the lungs</p>
       </div>
-  );
-}
+      <div className="list-item">
+        <p>Maintaining the work of the heart</p>
+      </div>
+      <div className="list-item">
+        <p>Release of processed substances</p>
+      </div>
+      <div className="list-item">
+        <p>Ensuring the stability of the internal environment</p>
+      </div>
+      <div className="list-item">
+        <p>Maintaining within the normal temperature</p>
+      </div>
+      <div className="list-item">
+        <p>Maintaining an immune system capable of resisting disease</p>
+      </div>
+    </div>
+  </div>
+);
+};
+
 
 export default WhyDrinkWater;
