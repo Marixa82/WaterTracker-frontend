c0601520a825c199b3139b8ca02e5038470b9917
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-18 21:25:49 +0200

    upd TrackerBenefits

--- src/components/Welcome/TrackerBenefits.tsx
+++ src/components/Welcome/TrackerBenefits.tsx
@@ -1,27 +1,41 @@
-// import { Container } from "react-bootstrap";
+import { REGISTER_ROUTE } from "../constants/routes";
 import Icon from "../Icon";
+import { NavLink } from "react-router-dom";
+import './Welcome.css'
 
 
-
-const TrackerBenefits: React.FC = () =>{
-  return(
-
-    <div className=" d-flex flex-wrap container-sm p-0">
-      <div className="card-border-0" >
-      <h1>WaterСonsumptionTracker</h1>
-      <h2>Record daily water intake and track</h2>
-      <h3 className='mb-2,5'>TrackerBenefitsList</h3>
-          <ul className="p-0">
-            <li className="d-flex align-items-center mb-3">
-              <Icon iconId="icon-calendar-days" width={32} height={32} color="inherit" />
-            <h3 className="ms-2 mb-0">Track your habits</h3>
-            </li>
-            <li className="d-flex align-items-center mb-3"><Icon iconId="icon-presentation-chart-bar" width={32} height={32} /><h3 className="ms-2 mb-0">View statistics</h3></li>
-            <li className="d-flex align-items-center"><Icon iconId="icon-eye-slash" width={32} height={32} /><h3 className="ms-2 mb-0">Personal rate setting</h3></li>
-          </ul>
+export const TrackerBenefits: React.FC = () => {
+  return (
+    <div className="tracker-container py-4">
+      <div className="mb-3">
+        <h1 className="display-5">Water Consumption Tracker</h1>
+        <p className="lead">Record daily water intake and track</p>
       </div>
-      <button type="button" className="btn btn-primary btn-lg w-100">Try tracker</button>
+
+      {/* Заголовок для Benefits */}
+      <h2 className="mb-3">Tracker Benefits</h2>
+
+      {/* Список підпунктів */}
+      <ul className="p-0">
+        <li className="d-flex align-items-center mb-2">
+          <Icon iconId="icon-calendar-days" width={32} height={32} color="inherit" />
+          <p className="ms-2 mb-0">Track your habits</p>
+        </li>
+        <li className="d-flex align-items-center mb-2">
+          <Icon iconId="icon-presentation-chart-bar" width={32} height={32} />
+          <p className="ms-2 mb-0">View statistics</p>
+        </li>
+        <li className="d-flex align-items-center mb-2">
+          <Icon iconId="icon-eye-slash" width={32} height={32} />
+          <p className="ms-2 mb-0">Personal rate setting</p>
+        </li>
+      </ul>
+
+      {/* Кнопка для спроби трекера */}
+      <NavLink to={REGISTER_ROUTE} className="auth-nav-link">
+        <button type="button" className="btn btn-primary btn-lg w-100">Try tracker</button>
+      </NavLink>
     </div>
-  )
-}
-export default TrackerBenefits;
+  );
+};
+
