89564a2a2635d0b3e01d352556566506451182a3
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-18 21:25:13 +0200

    upd logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,18 +1,19 @@
 import logoUrl from '../assets/LogoWater.svg';
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { HOME_ROUTE, WELCOME_ROUTE } from './constants/routes';
 interface LogoProps {
   isLoggedIn: boolean;
 }
 
 const Logo: React.FC<LogoProps> = ({ isLoggedIn }) => {
-  const navigate = useNavigate();
-  const redirectPath = isLoggedIn ? '/home' : '/';
+  
+  const redirectPath = isLoggedIn ? HOME_ROUTE : WELCOME_ROUTE;
 
   return (
-    <a href={redirectPath} onClick={() => navigate(redirectPath)}>
+    <NavLink to={redirectPath} className="d-flex align-items-center">
       <img src={logoUrl} alt="Logo" />
       <h1>TRACKER<br />OF WATER</h1>
-    </a>
+    </NavLink>
   );
 };
 
