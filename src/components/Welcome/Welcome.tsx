1ee0f9c548ccdf875fc9844ec2bfe58d9b3207af
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-20 19:21:28 +0200

    upd Welcome

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -7,13 +7,15 @@ interface WelcomeProps {
 
 function Welcome({ trackerBenefitsList, whyDrinkWater }: WelcomeProps) {
   return (
-    <div className='welcome-container py-5'>
-      < div className="row">
+    <div className='welcome-container my-5 '>
+      < div className="row welcome-item ">
         <div className="col-12 col-md-6">
           {trackerBenefitsList}
           </div>
-          <div className="col-12 col-md-6">
-          {whyDrinkWater}
+          <div className="col-12 col-md-6  d-flex flex-column flex-md-row  ">
+          <div className="why-drink-water">
+            {whyDrinkWater}
+            </div>
   
         </div>
       </div>
