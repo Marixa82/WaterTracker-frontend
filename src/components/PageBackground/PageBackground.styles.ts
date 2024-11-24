4afd9a67d62c2b8efad0d992dcec580455e2828f
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-24 23:08:08 +0200

    create PageBackground.styles.ts

---
+++ src/components/PageBackground/PageBackground.styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const BackgroundContainer = styled.div<{ desktopBackground: string; tabletBackground: string; mobileBackground: string }>`
+  height: 100vh;
+  width: 100vw;
+  background: ${({ desktopBackground }) => desktopBackground};
+  background-repeat: no-repeat, no-repeat;
+  background-size: cover, contain;
+  background-position: center, bottom;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+
+  /* Для планшетів */
+  @media (max-width: 1024px) {
+    background: ${({ tabletBackground }) => tabletBackground};
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+
+  /* Для мобільних */
+  @media (max-width: 768px) {
+    background: ${({ mobileBackground }) => mobileBackground};
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+`;
