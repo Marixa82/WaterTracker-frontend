1398a8213771db7e8839e9e37f50ccde00815cf3
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-25 18:01:44 +0200

    upd PageBackground

--- src/components/PageBackground/PageBackground.tsx
+++ src/components/PageBackground/PageBackground.tsx
@@ -11,9 +11,9 @@ interface PageBackgroundProps {
 export const PageBackground: React.FC<PageBackgroundProps> = ({ children }) =>{
   return(
     <BackgroundContainer
-      desktopBackground={backgrounds.desktop}
-      tabletBackground={backgrounds.tablet}
-      mobileBackground={backgrounds.mobile}
+      $desktopBackground={backgrounds.desktop}
+      $tabletBackground={backgrounds.tablet}
+      $mobileBackground={backgrounds.mobile}
     >
       {children}
     </BackgroundContainer>
