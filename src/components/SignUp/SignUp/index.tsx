d378b58aa366168a53b50a0221b155b2eff3b0d0
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-25 18:03:13 +0200

    upd index SignUp

--- src/components/SignUp/SignUp/index.tsx
+++ src/components/SignUp/SignUp/index.tsx
@@ -1,53 +1,80 @@
-import React from "react";
+import React, {FormEvent} from "react";
 import { useAppDispatch } from "../../../hooks/hook";
-import { authOperations } from '../../../redux/auth'
+import { authOperations } from '../../../redux/auth';
+import { Input, Button, FormViews, H2 } from "../../Forms/Formik/Formik.styled";
 
+interface FormElements extends HTMLFormControlsCollection {
+    userName: HTMLInputElement;
+    userEmail: HTMLInputElement;
+    userPassword: HTMLInputElement;
+  }
+  
+  interface FormEventExtended extends FormEvent<HTMLFormElement> {
+    currentTarget: HTMLFormElement & { elements: FormElements };
+  }
 
   
   const SignUp: React.FC = () => {
     const dispatch = useAppDispatch();
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (e: FormEventExtended): void => {
         e.preventDefault();
         const form = e.currentTarget;
-        const name = (form.elements.namedItem('userName') as HTMLInputElement).value;
-        const email = (form.elements.namedItem('userEmail') as HTMLInputElement).value;
-        const password = (form.elements.namedItem('userPassword') as HTMLInputElement).value;
-
         dispatch(authOperations.signUp({
-            name,
-            email,
-            password,
+          name: form.elements.userName.value,
+          email: form.elements.userEmail.value,
+          password: form.elements.userPassword.value,
         }));
-    }
+        form.reset();
+      };
+    // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     const form = e.currentTarget;
+    //     const name = (form.elements.namedItem('userName') as HTMLInputElement).value;
+    //     const email = (form.elements.namedItem('userEmail') as HTMLInputElement).value;
+    //     const password = (form.elements.namedItem('userPassword') as HTMLInputElement).value;
+
+    //     dispatch(authOperations.signUp({
+    //         name,
+    //         email,
+    //         password,
+    //     }));
+    // }
     return (
-        <form onSubmit={handleSubmit} autoComplete="off">
-            <label>
-                <span>
-                    <input type="text" name="userName" placeholder="Enter your name..." required />
-                </span>
-            </label>
-            <label>
-                <span>
-                    <input type="email" name="userEmail" placeholder="Enter your email..." required />
-                </span>
-            </label>
-            <label>
-                <span>
-                    <input
-                        type="password"
-                        name="userPassword"
-                        required
-                        placeholder="Enter your password..."
-                        // minLength="6"
-                        // maxLength="12"
-                        pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"
-                        title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number."
-                    />
-                </span>
-            </label>
-            <button type="submit" >Register</button>
-        </form>
+        <div>
+            
+            <FormViews onSubmit={handleSubmit} autoComplete="off">
+            <H2>Sign up</H2>
+                <label >
+                    <p>Enter your name</p>
+                    <span>
+                        <Input type="text" name="userName" placeholder="Enter your name..." required />
+                    </span>
+                </label>
+                <label>
+                    <p>Enter your email</p>
+                    <span>
+                        <Input type="email" name="userEmail" placeholder="Enter your email..." required />
+                    </span>
+                </label>
+                <label>
+                    <p>Enter your password</p>
+                    <span>
+                    <Input
+                type="password"
+                name="userPassword"
+                required
+                placeholder="Enter your password..."
+                minLength={6}
+                maxLength={12}
+                pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"
+                title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number."
+              />
+                    </span>
+                </label>
+                <Button type="submit" >Register</Button>
+            </FormViews>
+        </div>
     );
   };
   export default SignUp
