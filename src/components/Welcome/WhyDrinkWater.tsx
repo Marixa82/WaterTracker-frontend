10b85b74624f1b28ac6965048a7b6b7ae134221b
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-11-25 18:04:26 +0200

    upd WhyDrinkWater

--- src/components/Welcome/WhyDrinkWater.tsx
+++ src/components/Welcome/WhyDrinkWater.tsx
@@ -4,41 +4,44 @@ import './Welcome.css'
 
 const WhyDrinkWater: React.FC = () => {
   return (
-    <div className="container ">
-      <div className="why-drink-container">
-        <h3 className="why-drink-water-title">Why Drink Water</h3>
-        <div className="list-container ">
-          <div className="list-item">
-            <span className="dot"></span>
-            <p>Supply of nutrients to all organs</p>
+    <div className="why-drink-container ">
+      <div className="why-drink-water-title">
+    
+          <h3 >Why Drink Water</h3>
           </div>
-          <div className="list-item">
-            <span className="dot"></span>
-            <p>Providing oxygen to the lungs</p>
+          <div className="list-container ">
+            <div className="list-item">
+              <span className="dot"></span>
+              <p>Supply of nutrients to all organs</p>
+            </div>
+            <div className="list-item">
+              <span className="dot"></span>
+              <p>Providing oxygen to the lungs</p>
+            </div>
+            <div className="list-item">
+              <span className="dot "></span>
+              <p>Maintaining the work of the heart</p>
+            </div>
+            <div className="list-item">
+              <span className="dot "></span>
+              <p>Release of processed substances</p>
+            </div>
+            <div className="list-item">
+              <span className="dot "></span>
+              <p>Ensuring the stability of the internal environment</p>
+            </div>
+            <div className="list-item">
+              <span className="dot "></span>
+              <p>Maintaining within the normal temperature</p>
+            </div>
+            <div className="list-item">
+              <span className="dot"></span>
+              <p>Maintaining an immune system capable of resisting disease</p>
+            </div>
           </div>
-          <div className="list-item">
-            <span className="dot "></span>
-            <p>Maintaining the work of the heart</p>
-          </div>
-          <div className="list-item">
-            <span className="dot "></span>
-            <p>Release of processed substances</p>
-          </div>
-          <div className="list-item">
-            <span className="dot "></span>
-            <p>Ensuring the stability of the internal environment</p>
-          </div>
-          <div className="list-item">
-            <span className="dot "></span>
-            <p>Maintaining within the normal temperature</p>
-          </div>
-          <div className="list-item">
-            <span className="dot"></span>
-            <p>Maintaining an immune system capable of resisting disease</p>
-          </div>
-        </div>
       </div>
-    </div>
+     
+    
   );
 };
 
